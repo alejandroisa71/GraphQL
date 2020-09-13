9774e113ba5341a653cdfe8a0a20325f3dc41be6
Alejandro Isa <alejandroisa71@gmail.com>
2020-09-12 23:30:51 -0300

    se agrega type Usuario y type Mutation en schema.js y se lo llama a Mutation desde resolvers.js

--- db/resolvers.js
+++ db/resolvers.js
@@ -4,6 +4,9 @@ const resolvers = {
   Query: {
     obtenerCurso: () => "Algo",
   },
+  Mutation: {
+    nuevoUsuario: () => "Creando Nuevo Usuario",
+  },
 };
 
 module.exports = resolvers;
--- db/schema.js
+++ db/schema.js
@@ -2,8 +2,18 @@ const { gql } = require("apollo-server");
 
 //Schema.
 const typeDefs = gql`
+  type Usuario {
+    id: ID
+    nombre: String
+    apellido: String
+    email: String
+    creado: String
+  }
   type Query {
     obtenerCurso: String
+  }
+  type Mutation {
+    nuevoUsuario: String
   }
 `;
 module.exports = typeDefs;
